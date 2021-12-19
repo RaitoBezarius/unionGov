6e41752abd589d30f9324dc58e433d50466b2434
Raito Bezarius <masterancpp@gmail.com>
2021-12-19 18:31:24 +0100

    frontend(sort): sort properly

--- frontend/src/lib/sort.ts
+++ frontend/src/lib/sort.ts
@@ -10,10 +10,10 @@ export function getLastName (name: string) {
 }
 
 export function sortByLastName (left: Candidate, right: Candidate) {
-  if (right.label === SOMEONE_ELSE || left.label === SOMEONE_ELSE) {
-    return left.label === right.label ? 0 : 1;
-  } else if (left.label === SOMEONE_ELSE) {
+  if (right.label === SOMEONE_ELSE) {
     return left.label === right.label ? 0 : -1;
+  } else if (left.label === SOMEONE_ELSE) {
+    return left.label === right.label ? 0 : 1;
   } else {
     return getLastName(left.label).localeCompare(getLastName(right.label));
   }
