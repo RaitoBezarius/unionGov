3990eadd98b64c3a8769c298e2621ec53a8c3fe5
Raito Bezarius <masterancpp@gmail.com>
2021-12-21 18:22:58 +0100

    chore: prepare for candidate slot

--- frontend/src/components/CandidateItem/index.tsx
+++ frontend/src/components/CandidateItem/index.tsx
@@ -1,16 +1,19 @@
 import { Typography } from '@material-ui/core';
 import { FunctionComponent, memo } from 'react';
+import Image from 'next/image';
+import SlotImage from './candidate-slot.png';
 
 export type Props = {
   imageUrl: string;
   label: string;
 };
 
+// TODO: put slot image behind the image of the candidate, using positive rel + zindex.
 /** Displays a candidate list item */
 const CandidateItem: FunctionComponent<Props> = memo(({ imageUrl, label }) => (
   <li className="flex-fill list-group-item d-flex justify-content-between align-items-center">
     <Typography className="col-10">{label}</Typography>
-    <img className="col-2" src={imageUrl} alt={label} />
+    {/* <Image src={SlotImage} alt='Hexagone jaune pour accueillir les images de candidats' layout='responsive' className="" /> */}
   </li>
 ));
 CandidateItem.displayName = 'CandidateItem';
