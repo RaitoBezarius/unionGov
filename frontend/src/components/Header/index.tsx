7ac1b2b09de358053e1b95f10d2b35c8bc93908f
Yvan Sraka <yvan@sraka.xyz>
2021-12-27 19:11:37 +0100

    Fix issue #26

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,8 @@
+import Image from 'next/image';
 import { CSSProperties, FunctionComponent, memo } from 'react';
 import theme from '../../theme';
 import { EmptyRecord } from '../../types';
 import HeaderImage from './header.png';
-import Image from 'next/image';
 
 type Styles = {
   container: CSSProperties;
@@ -23,7 +23,7 @@ const styles: Styles = {
 
 const Header: FunctionComponent<EmptyRecord> = memo(() => (
   <div style={styles.container} className="p-4">
-    <a href="https://plusbellelapolitique.fr" target="_blank" rel="noreferrer">
+    <a href="https://charlotte-marchandise.fr" target="_blank" rel="noreferrer">
       <Image src={HeaderImage} />
     </a>
     {/* <Typography style={styles.text} variant="h5">À nous de choisir !</Typography> */}
